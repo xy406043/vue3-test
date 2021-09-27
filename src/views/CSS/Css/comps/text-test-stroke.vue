ae240fb405efc18881054d9cb79ad1249073fa19
xuyuan <xy@yuxisoft.cn>
2021-09-27 11:27:41 +0800

    :sparkles: 引用自己的npm包

--- src/views/CSS/Css/comps/text-test-stroke.vue
+++ src/views/CSS/Css/comps/text-test-stroke.vue
@@ -26,7 +26,12 @@
 <script setup lang="ts">
 // 参考： https://juejin.cn/post/7010944239609577508
 // 字体子集化： http://ecomfe.github.io/fontmin/#source
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import xyTools from 'xy-use-tools'
+onMounted(() => {
+  let time = ref<boolean>(false)
+  console.log('获取最新的数据', xyTools)
+})
 </script>
 
 <style lang="less" scoped>
